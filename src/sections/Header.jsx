31fcc2c835561b9083c5a940c2281f16c3e59317
amanantino <aman.singh@antino.com>
2025-02-27 11:27:10 +0530

    Completed Features Section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,20 +1,36 @@
 import { Link as LinkScroll } from "react-scroll"
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import clsx from "clsx";
-const NavLink = ({ title }) => (
-    <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5">
-        {title}
-    </LinkScroll>
-)
 
-NavLink.propTypes = {
-    title: PropTypes.string.isRequired,
-};
 const Header = () => {
+    const [hasScrolled, sethasScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            sethasScrolled(window.scrollY > 32)
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [])
+
+    const NavLink = ({ title }) => (
+        <LinkScroll to={title} offset={-100} spy smooth activeClass="nav-active" onClick={()=>setisOpen(false)} className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h-5">
+            {title}
+        </LinkScroll>
+    )
+
+    NavLink.propTypes = {
+        title: PropTypes.string.isRequired,
+    };
+
+
     const [isOpen, setisOpen] = useState(false)
+
     return (
-        <header className="fixed top-0 left-0 z-50 w-full py-10">
+        <header className={clsx("fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4", hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]")}>
             <div className="container flex h-14 items-center max-lg:px-5">
                 <a className="lg:hidden flex-1 cursor-pointer z-2">
                     <img src="/images/xora.svg" height={55} width={115} alt="" />
@@ -29,7 +45,7 @@ const Header = () => {
                                     <NavLink title={"Pricing"} />
                                 </li>
                                 <li className="nav-logo">
-                                    <LinkScroll to="hero" offset={-100} spy smooth className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}>
+                                    <LinkScroll to="Hero" offset={-250} spy smooth className={clsx("max-lg:hidden transition-transform duration-500 cursor-pointer")}>
                                         <img src="/images/xora.svg" alt="Logo" height={55} width={160} />
                                     </LinkScroll>
                                 </li>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "../components/Button"
 const Hero = () => {
     return (
         <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
-            <Element name="hero">
+            <Element name="Hero">
                 <div className="container">
                     <div className="relative z-2 max-w-512 max-lg:max-w-388">
                         <div className="caption small-2 uppercase text-p3">
